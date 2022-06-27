ecad7c8776f08a656900a33c3772040e0916efbd
mdtamizuddin <tamizrabbi@gmail.com>
2022-06-28 00:31:11 +0600

    Subscribe newsletter added

--- src/Components/Shared/Subscrive.js
+++ src/Components/Shared/Subscrive.js
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import NewsLatter from '../Home/images/Newsletter.svg'
 const Subscrive = () => {
     return (
-        <div className='h-40 bg-primary w-full'>
-            
+        <div className='py-10 my-20 bg-primary w-full flex items-center'>
+            <div className='flex h-full items-center container mx-auto justify-between'>
+                <div className='flex items-center'>
+                    <img style={{ height: '80%' }} src={NewsLatter} alt="" />
+                    <div className='ml-4'>
+                        <h1 className='text-4xl font-bold text-white'>Join our newsletter now!</h1>
+                        <p className='text-gray-200 mt-2'>Register now and get our latest updaye and promos</p>
+                    </div>
+                </div>
+
+                {/* left Side End  */}
+                <div >
+                    <input type="text" placeholder="Enter You Email" className="input  rounded-none rounded-tl-lg rounded-bl-lg   w-96" />
+                    <button className='btn rounded-none rounded-tr-lg rounded-br-lg'>Join</button>
+                </div>
+
+            </div>
         </div>
     )
 }
