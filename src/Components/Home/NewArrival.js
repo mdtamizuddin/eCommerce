b286e7ad0f20ff5c325ba205128217180dc1cfe0
mdtamizuddin <tamizrabbi@gmail.com>
2022-06-27 14:10:15 +0600

    HOme page New Arrival Section Added

---
+++ src/Components/Home/NewArrival.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewArrival = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">New Arrival</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'></div>
+        </div>
+    )
+}
+
+export default NewArrival
