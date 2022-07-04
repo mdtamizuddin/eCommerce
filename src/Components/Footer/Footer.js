7bc539d96e298a99358f79bb6571ff81a4be6f0a
mdtamizuddin <tamizrabbi@gmail.com>
2022-07-05 01:48:21 +0600

    Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
 
         <footer className="text-gray-600 body-font">
-            <div className="container px-5 lg:py-24 mx-auto flex md:items-center lg:items-start md:flex-col md:flex-nowrap flex-wrap flex-col">
+            <div className="container px-5 lg:py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
                 <div className="lg:w-96 w-full flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
                     <a href="!#" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                         <span className="mb-2 text-xl">
